6067b8986bb9adcb9b021beeb87bb753812a5205
Christopher Carman <chris@thecarmans.com>
2018-01-31 21:56:18 +0100

    playing around

--- Chris/pineapple.m
+++ Chris/pineapple.m
@@ -10,3 +10,14 @@ N = 1;
 e = 0.0001;
 %% 
 
+naboer = [1, 2, 3, 10 , -10 , -4, 2 , 3, 3, 2 ];
+nb = sort(naboer);
+
+nbb = nb(1);
+i1=1; 
+for i = 2:length(nb)   
+    if nb(i1)~= nb(i)
+        nbb=[nbb, nb(i)];
+        i1=i;
+    end
+end
