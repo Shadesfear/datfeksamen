0ae0101a175db7de7d53ca4d140283ab8266c31d
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2018-02-03 17:25:03 +0100

    Tjek alle klodser funktion færdig. mangler nabo check nu

--- Morten/klodsclass.m
+++ Morten/klodsclass.m
@@ -10,40 +10,47 @@ classdef klodsclass
        pos = [];
        dpos = [0,0];
        dplade = [0,0];
+       F_hori = [0,0];
+       stable;
+       flyttet;
+       Econt = 0;
+       L;
        
     end
     
     methods
-        function obj = klodsclass(l_0, kx, ky, k_p)
+        function obj = klodsclass(l_0, l_p, kx, ky, k_p)
             obj.fjederv = [l_0, kx];
             obj.fjederh = [l_0, kx];
             obj.fjedero = [l_0, ky];
             obj.fjedern = [l_0, ky];
-            obj.k_p = k_p
+            obj.k_p = k_p;
+            obj.l_p = l_p;
         end
         
             
         function kraft = stable2d(oa)
             
             [x,y] = size(oa);
-
+            kraft = zeros(x,y);
+            size(kraft)
             for i=(1:x)
-                
                 for m=(1:y)
                     i_op = i-1;
                     i_ned = i+1;
                     m_v = m-1;
                     m_h = m+1;
+                    objee = oa(i,m);
                     
                     if(i == 1)
-                       i_op = size(oa,1);
+                       i_op = x;
                        
                     elseif(i == x)
                         i_ned = 1;
                     end
                     
                     if(m == 1)
-                        m_v = size(oa,2);
+                        m_v = y;
                         
                     elseif(m == y) 
                         m_h = 1;
@@ -51,12 +58,12 @@ classdef klodsclass
                     end
                     
 
-                    kraft = oa(i,m).k_p*oa(i,m).l_p + oa(i,m).fjederv(1,2)*(oa(i,m_v).pos - oa(i,m).pos)
-                    + oa(i,m).fjederh(1,2) * (oa(i,m_h).pos - oa(i,m).pos)
-                    + oa(i,m).fjedern(1,2) * (oa(i_ned,m).pos - oa(i,m).pos)
-                    + oa(i,m).fjedero(1,2) * (oa(i_op,m).pos - oa(i,m).pos);
-                    
-                    
+                    kraft = oa(i,m).k_p * oa(i,m).l_p...
+                        + oa(i,m).fjederv(1,2) * (oa(i,m_v).pos - oa(i,m).pos)...
+                        + oa(i,m).fjederh(1,2) * (oa(i,m_h).pos - oa(i,m).pos)...
+                        + oa(i,m).fjedern(1,2) * (oa(i_ned,m).pos - oa(i,m).pos)...
+                        + oa(i,m).fjedero(1,2) * (oa(i_op,m).pos - oa(i,m).pos);
+                    disp(kraft(i,m))
                 end
                 
             end
